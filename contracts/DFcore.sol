3588e0ca2be0f183ccda468c42a9d31428b47516
Pion <guitarfullife@gmail.com>
2018-10-30 16:45:40 +0900

    truffle test add

--- contracts/DFcore.sol
+++ contracts/DFcore.sol
@@ -3,7 +3,7 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract DFcore is Ownable {
 
-  struct Box {
+  struct PJ {
     string purpose;
     uint target;
     uint amount;
@@ -12,68 +12,68 @@ contract DFcore is Ownable {
     mapping (address => uint) funds;  //支援者の誰がどれだけ投げてくれたのかを記録しておく
   }
 
-  Box[] public Boxs;
-  mapping (uint => address) public BoxToOwner;
-  mapping (address => uint) public ownerBoxCount;
+  PJ[] public PJs;
+  mapping (uint => address) public PJToOwner;
+  mapping (address => uint) public ownerPJCount;
 
-  modifier onlyOwnerOf(uint _BoxId) {
-    require(msg.sender == BoxToOwner[_BoxId]);
+  modifier onlyOwnerOf(uint _PJId) {
+    require(msg.sender == PJToOwner[_PJId]);
     _;
   }
 
-  function makeBox(string _purpose, uint _target) public {  // クラウドファンディングのETHを貯める箱を作る関数
+  function makePJ(string _purpose, uint _target) public {  // クラウドファンディングのETHを貯める箱を作る関数
     uint _limittime = now + 30 days; //とりあえずデフォルトで期限を30日と設定
     address[] memory _supportersArray;
-    uint id = Boxs.push(Box(_purpose,_target, 0, _limittime, _supportersArray)) - 1;
-    BoxToOwner[id] = msg.sender;
-    ownerBoxCount[msg.sender]++;
+    uint id = PJs.push(PJ(_purpose,_target, 0, _limittime, _supportersArray)) - 1;
+    PJToOwner[id] = msg.sender;
+    ownerPJCount[msg.sender]++;
   }
 
   function deposit(uint _id) public payable { // 箱にETHを投げる関数
-    require(Boxs[_id].amount < Boxs[_id].target);
-    require(now <= Boxs[_id].limittime);
-    Boxs[_id].amount = Boxs[_id].amount + msg.value;
-    Boxs[_id].supportersArray.push(msg.sender);
-    Boxs[_id].funds[msg.sender] = msg.value;
+    require(PJs[_id].amount < PJs[_id].target);
+    require(now <= PJs[_id].limittime);
+    PJs[_id].amount = PJs[_id].amount + msg.value;
+    PJs[_id].supportersArray.push(msg.sender);
+    PJs[_id].funds[msg.sender] = msg.value;
   }
 
-  function success_withdraw(uint _id) public onlyOwnerOf(_id) {  // 箱の中が満額以上の時のみ、Box製作者がのみが実行してお金を引き出すことができる
-    require(Boxs[_id].amount >= Boxs[_id].target);
-    uint nakami = Boxs[_id].amount;
-    Boxs[_id].amount = 0;
-    ownerBoxCount[BoxToOwner[_id]]--;
-    BoxToOwner[_id] = owner; //ownerはコントラクト作成者
-    ownerBoxCount[owner]++;
+  function success_withdraw(uint _id) public onlyOwnerOf(_id) {  // 箱の中が満額以上の時のみ、PJ製作者がのみが実行してお金を引き出すことができる
+    require(PJs[_id].amount >= PJs[_id].target);
+    uint nakami = PJs[_id].amount;
+    PJs[_id].amount = 0;
+    ownerPJCount[PJToOwner[_id]]--;
+    PJToOwner[_id] = owner; //ownerはコントラクト作成者
+    ownerPJCount[owner]++;
     msg.sender.transfer(nakami);
   }
 
   function failure_withdraw(uint _id) public {  // 期限を超えて、目標額集まらなかった時に、貯めたお金を支援者に返金する
-    require(now > Boxs[_id].limittime);
-    require(Boxs[_id].amount < Boxs[_id].target);
-    for (uint i = 0; i < Boxs[_id].supportersArray.length; i++) {
-      address supporteraddress = Boxs[_id].supportersArray[i];
-      uint siengaku = Boxs[_id].funds[supporteraddress];
-      Boxs[_id].funds[supporteraddress] = 0;
+    require(now > PJs[_id].limittime);
+    require(PJs[_id].amount < PJs[_id].target);
+    for (uint i = 0; i < PJs[_id].supportersArray.length; i++) {
+      address supporteraddress = PJs[_id].supportersArray[i];
+      uint siengaku = PJs[_id].funds[supporteraddress];
+      PJs[_id].funds[supporteraddress] = 0;
       supporteraddress.transfer(siengaku);
     }
   }
 
-  function getBoxByOwner(address _owner) external view returns(uint[]) {
-    uint[] memory result = new uint[](ownerBoxCount[_owner]);
+  function getPJByOwner(address _owner) external view returns(uint[]) {
+    uint[] memory result = new uint[](ownerPJCount[_owner]);
     uint counter = 0;
-    for (uint i = 0; i < Boxs.length; i++) {
+    for (uint i = 0; i < PJs.length; i++) {
       result[counter] = i;
       counter++;
     }
     return result;
   }
 
-  function getBox(uint _id) public view returns(string, uint256, uint256, uint256, address[]) {
-    return (Boxs[_id].purpose, Boxs[_id].target, Boxs[_id].amount, Boxs[_id].limittime, Boxs[_id].supportersArray);
+  function getPJInfo(uint _id) public view returns(string, uint256, uint256, uint256, address[]) {
+    return (PJs[_id].purpose, PJs[_id].target, PJs[_id].amount, PJs[_id].limittime, PJs[_id].supportersArray);
   }
 
-  function getBoxfunds(uint _id, address _address) public view returns(uint256) {
-    return (Boxs[_id].funds[_address]);
+  function getPJFunds(uint _id, address _address) public view returns(uint256) {
+    return (PJs[_id].funds[_address]);
   }
 
 }
--- test/DFcore.js
+++ test/DFcore.js
@@ -1,44 +1,84 @@
 var DFcore = artifacts.require("./DFcore.sol");
 
 contract('DFcore', function(accounts) {
-  it("make box", function() {
+  it("After makePJ(), accounts[0]'s ownerPJCount should be added 1", function() {
+    var core;
+    var count;
+    return DFcore.deployed().then(function(instance) {
+      core = instance;
+      return core.ownerPJCount.call(accounts[0]);
+    }).then(function(result) {
+      count = result.c[0];
+    }).then(function() {
+      return core.makePJ('日本を変える', 50000);
+    }).then(function() {
+      return core.ownerPJCount.call(accounts[0]);
+    }).then(function(result) {
+      return assert.equal(count+1, result.c[0], "accounts[0]'s ownerPJCount should 1")
+    });
+  });
+
+  it("After deposit(), PJs[0]'s amount should be added", function() {
     var core;
     return DFcore.deployed().then(function(instance) {
       core = instance;
-      return core.makeBox('寝たままで暮らしたい', 50000);
-    }).then(function(){
-      return core.BoxToOwner.call(0);
+      return core.deposit(0, {value: web3.toWei(4, 'ether')});
+    }).then(function() {
+      return core.getPJInfo(0);
     }).then(function(result) {
-      assert.equal(result, accounts[0], "1st box's owener isn't account[0]");
+      return assert.equal(result[2], web3.toWei(4, 'ether'), "PJs[0]'s amount should 4 ether");
     });
   });
+
+  it("満額お金が貯まっている際に、引き出しを行うと、PJ作成者に送金されるか", function() {
+    
+  })
 
   it("mapping 両方確認", function() {
     var core;
     return DFcore.deployed().then(function(instance) {
       core = instance;
-      return core.BoxToOwner.call(0);
+      return core.PJToOwner.call(0);
     }).then(function(result) {
-      console.log("Box 0's owner : " + result);
-      return core.ownerBoxCount.call(result);
+      console.log("PJ 0's owner : " + result);
+      return core.ownerPJCount.call(result);
     }).then(function(num) {
-      console.log("address box num : " + num);
-    });
-  });
-
-  it("deposit", function() {
-    var core;
-    return DFcore.deployed().then(function(instance) {
-      core = instance;
-      return core.deposit(0, {value: web3.toWei(5, 'ether')});
-    }).then(function() {
-      return core.getBox(0);
-    }).then(function(result) {
-      console.log(result);
-    }).then(function() {
-      return core.getBoxfunds(0, accounts[0]);
-    }).then(function(result) {
-      console.log(accounts[0]+ " が投げた額は " + result);
+      console.log("address PJ num : " + num);
     });
   });
 });
+//
+// ・プロジェクトの作成ができているかどうか
+// 　プロジェクトを作る。
+// 　プロジェクトの中身が作ったものと一致しているかどうか
+//
+// ・プロジェクトの中にちゃんと指定した額を投げることができるかどうか
+// 　プロジェクトにお金を投げる
+// 　プロジェクトの中の残高を確認する
+// 　それらが一致しているかどうか
+//
+// ・満額お金が貯まっている時に、ちゃんとプロジェクト作成者に送られているか
+// 　まず、プロジェクトを満額以上にする
+// 　満額以上になっていることを確認する
+// 　success_withdrawを実行させる
+// 　貯まっていたお金だけ、プロジェクト主の残高が増えているか確認する
+//
+// ・満額お金が貯まっていない時に、支援者に全額のお金が送られているか
+// 　まずプロジェクトに満額以下のお金を貯める
+// 　満額以下のお金が入っていることを確認する
+// 　支援者がそれぞれどれだけの額を支援しているのかを確認する
+// 　failure_withdrawを実行させる(limittimeを過ぎていることも条件であることを忘れないように)
+// 　支援者それぞれに、支援した額が返ってきていることを確認する
+//
+// ・アドレスに合わせて、その人が作ったプロジェクトのidを取得する
+// 　getBoxByOwnerの関数を叩く
+// 　配列が返ってきていることを確認する
+//
+// ・プロジェクトidに合わせて、プロジェクト名、目標額、合計金額、制限時間、支援者のアドレス配列を取得する
+// 　getBoxInfoの関数を叩く
+// 　作成した時の情報が返ってきていることを確認する
+//
+// ・あるプロジェクトに、ある人がどれだけ支援しているのかの情報を取得
+// 　getBoxFunds関数にプロジェクトidとaddressを渡して、その情報があっているか確認する
+//
+// ・mappingのBoxToOwnerとowenrBoxCountが正しい値を返すかどうかを確認する
